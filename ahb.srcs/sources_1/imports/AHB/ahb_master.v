9a612ae08c2cc02477aefe37dcbdae7ad11951b2
ransaraw98 <ransarawijitha@gmail.com>
2023-03-21 01:11:22 +0530

    Removed unsed I/O from master.

--- ahb.srcs/sources_1/imports/AHB/ahb_master.v
+++ ahb.srcs/sources_1/imports/AHB/ahb_master.v
@@ -17,8 +17,8 @@ module ahb_master(
   //input [31:0] dinb,
   input [31:0] addr,
   input wr,
-  input hreadyout,
-  input hresp,
+  //input hreadyout,
+  //input hresp,
   input [31:0] hrdata,
   input [1:0] slave_sel,
   input hgrant,
@@ -26,11 +26,11 @@ module ahb_master(
   output reg [1:0] sel,
   output reg [31:0] haddr,
   output reg hwrite,
-  output reg [2:0] hsize,
+  //output reg [2:0] hsize,
   output reg [2:0] hburst,
-  output reg [3:0] hprot,
-  output reg [1:0] htrans,
-  output reg hmastlock,
+  //output reg [3:0] hprot,
+  //output reg [1:0] htrans,
+  //output reg hmastlock,
   output reg hready,
   output reg [31:0] hwdata,
   output reg [31:0] dout,
@@ -120,11 +120,11 @@ always@(posedge hclk, negedge hresetn) begin
     sel <= 2'b00;
     haddr <= 32'h0000_0000;
     hwrite <= 1'b0;
-    hsize <= 3'b000;
+    //hsize <= 3'b000;
     hburst <= 3'b000;
-    hprot <= 4'b0000;
-    htrans <= 2'b00;
-    hmastlock <= 1'b0;
+    //hprot <= 4'b0000;
+    //htrans <= 2'b00;
+    //hmastlock <= 1'b0;
     hready <= 1'b0;
     hwdata <= 32'h0000_0000;
     dout <= 32'h0000_0000;
