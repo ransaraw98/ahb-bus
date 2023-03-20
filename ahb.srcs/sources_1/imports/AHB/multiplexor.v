c829725879e0c8a1afccf23623b344e3134dc419
ransaraw98 <ransarawijitha@gmail.com>
2023-03-20 16:57:16 +0530

    Added hresp signals to multiplexor.
    Multiplexor default state gives tr_done=0!.

--- ahb.srcs/sources_1/imports/AHB/multiplexor.v
+++ ahb.srcs/sources_1/imports/AHB/multiplexor.v
@@ -6,34 +6,42 @@
 //---------------------------------------------------
 
 module multiplexor(
-  input [31:0] hrdata_1,
-  input [31:0] hrdata_2,
-  input [31:0] hrdata_3,
-  input hreadyout_1,
-  input hreadyout_2,
-  input hreadyout_3,
+  input [31:0] hrdata1,
+  input [31:0] hrdata2,
+  input [31:0] hrdata3,
+  input hreadyout1,
+  input hreadyout2,
+  input hreadyout3,
+  input hresp1,
+  input hresp2,
+  input hresp3,
   input [1:0] sel,
   output reg [31:0] hrdata,
-  output reg hreadyout
+  output reg hreadyout,
+  output reg hresp
 );
 
 always@(*) begin
   case(sel)
     2'b01: begin
-      hrdata = hrdata_1;
-      hreadyout = hreadyout_1;
+      hrdata = hrdata1;
+      hreadyout = hreadyout1;
+      hresp = hresp1;
     end
     2'b10: begin
-      hrdata = hrdata_2;
-      hreadyout = hreadyout_2;
+      hrdata = hrdata2;
+      hreadyout = hreadyout2;
+      hresp = hresp2;
     end
     2'b11: begin
-      hrdata = hrdata_3;
-      hreadyout = hreadyout_3;
+      hrdata = hrdata3;
+      hreadyout = hreadyout3;
+      hresp = hresp3;
     end
     default: begin
       hrdata = 32'h0000_0000;
       hreadyout = 1'b0;
+      hresp = 1'b1;
     end
   endcase
 end
