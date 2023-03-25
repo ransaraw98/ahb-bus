7e79df5d71de2a067ce983a824a92e4326ed9ccd
ransaraw98 <ransarawijitha@gmail.com>
2023-03-25 21:39:08 +0530

    Revert "Added deploy_bus/deploy/deploy.srcs"
    This reverts commit 327ecfda1b7bcb7083bd92b7e47636f5c46db548.

--- ahb.srcs/sources_1/imports/AHB/ahb_slave.v
+++ ahb.srcs/sources_1/imports/AHB/ahb_slave.v
@@ -325,7 +325,8 @@ always @(posedge hclk, negedge hresetn) begin
       idle: begin
         hreadyout <= 1'b0;
         hresp <= 1'b0;
-        hrdata <= 32'h0000_0000;
+        //hrdata <= 32'h0000_0000;
+        hrdata <= hrdata;
         //waddr <= waddr;
         //raddr <= raddr;
       end
